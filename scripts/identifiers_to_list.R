af7defec3cabd668acb6ddd0647f77a1887e22ce
rtraborn <rtraborn@indiana.edu>
2017-11-06 16:31:57 -0500

    Added conversion script for creating list from 1000GP identifiers.

---
+++ scripts/identifiers_to_list.R
@@ -0,0 +1,21 @@
+
+table_to_list <- function(csv.file) {
+    if (is.character(csv.file)==FALSE) {
+        stop("csv.file must be of class character")
+    }
+        this.file <- read.csv(file=csv.file, header=TRUE)
+        this.table <- cbind(as.character(this.file[,1]), as.character(this.file[,3]))
+        these.pops <- unique(this.table[,2])
+        print(length(these.pops))
+        this.list <- vector(mode="list", length=length(these.pops))
+        names(this.list) <- these.pops
+        for (i in 1:length(these.pops)) {
+            this.pop <- these.pops[i]
+            this.ind <- which(this.table[,2]==this.pop)
+            my.indvs <- this.table[this.ind,1]
+            print(head(my.indvs))
+            this.list[[i]] <- my.indvs
+        }
+        return(this.list)
+}
+        
