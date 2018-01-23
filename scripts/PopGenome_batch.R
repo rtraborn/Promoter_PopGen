cc827f5560a31fdccf6580c4e564a78ecf297064
rtraborn <rtraborn@indiana.edu>
2018-01-22 22:37:29 -0500

    Updated PopGenome_batch.R after finding solution to R package installation problems.

--- scripts/PopGenome_batch.R
+++ scripts/PopGenome_batch.R
@@ -1,29 +1,32 @@
 ## This script in progress for testing on Carbonate
-#must install package 'ff' prior to installing PopGenome on the system
+#must add routine to do with gff3 files
 library(PopGenome)
 library(bigmemory)
 library(tools)
 
-source("id_to_list.R")
-
 ##########################################################################
-vcfDir <- "/N/dc2/projects/PromoterPopGen/human/human-split-data"	 #
-fileList <- "human_file_list.txt"					 #
-file.names <- read.csv(file=fileList, header=FALSE)			 #
-colnames(file.names) <- c("chr", "start", "end", "file")		 #
-gffFile <- "hg19.cage_peak_phase1and2combined_coord_chr22_reheader.gff3" #
-popListFile <- "pop_list_1kGP.csv"					 #
+vcfDir <- "/N/dc2/projects/PromoterPopGen/human/human-split-data"	 
+fileList <- "/N/dc2/projects/PromoterPopGen/human/human-split-data/human_file_list.txt"					 
+file.names <- read.csv(file=fileList, header=FALSE)
+colnames(file.names) <- c("chr", "start", "end", "file")		 
+gffFile <- "hg19.cage_peak_phase1and2combined_coord_chr22_reheader.gff3"
+popListFile <- "/N/dc2/projects/PromoterPopGen/DmPromoterPopGen/data/human/pop_list_1kGP.csv"					 
 ##########################################################################
 
 setwd(vcfDir)
+print(head(file.names))
+
+source("/N/dc2/projects/PromoterPopGen/DmPromoterPopGen/scripts/identifiers_to_list.R")
 pop.list <- identifiers_to_list(csv.file=popListFile)
 
-for (i in 1:nrow(file.names)) {
+#for (i in 1:nrow(file.names)) {
+for (i in 1:5) {
+    print(i)
     this.string <- file.names[i,]
     this.chr <- as.character(this.string[1])
     this.start <- this.string[2]
     this.end <- this.string[3]
-    this.filename <- as.character(this.string[4])
+    this.filename <- as.character(unlist(this.string[4]))
     this.filename2 <- file_path_sans_ext(this.filename)
     diversity_out <- paste(this.filename2, "diversity", sep="_")
     diversity_filename <- paste(diversity_out, "txt", sep=".")    
@@ -34,13 +37,13 @@ for (i in 1:nrow(file.names)) {
     print(this.end)
     print(this.filename)
 
-    #GENOME.class <- readVCF(filename=thisFileName, numcols=100000, frompos=this.start, topos=this.end, tid=this.chr, gffpath=gffFile)
-    #GENOME.class <- set.populations(GENOME.class, new.populations=pop.list, diploid=TRUE)  
-    #split <- split_data_into_GFF_features(GENOME.class, gff.file=gffFile, chr=this.chr, feature="sequence_feature")
-    #split <- diversity.stats(split, pi=TRUE, keep.site.info=TRUE)
-    #nuc.diversity.m <- split@nuc.diversity.within
-    #colnames(nuc.diversity.m) <- names(pop.list)
-    #write.table(nuc.diversity.m, col.names=TRUE, row.names=FALSE, sep="\t", file=diversity_filename) 
+    GENOME.class <- readVCF(filename=thisFileName, numcols=100000, frompos=this.start, topos=this.end, tid=this.chr, gffpath=gffFile)
+    GENOME.class <- set.populations(GENOME.class, new.populations=pop.list, diploid=TRUE)  
+    split <- split_data_into_GFF_features(GENOME.class, gff.file=gffFile, chr=this.chr, feature="sequence_feature")
+    split <- diversity.stats(split, pi=TRUE, keep.site.info=TRUE)
+    nuc.diversity.m <- split@nuc.diversity.within
+    colnames(nuc.diversity.m) <- names(pop.list)
+    write.table(nuc.diversity.m, col.names=TRUE, row.names=FALSE, sep="\t", file=diversity_filename) 
 
     #GENOME.class <- readVCF(filename=thisFileName, numcols=100000, frompos=this.start, topos=this.end, tid=this.chr, gffpath=gffFile)
     #GENOME.class <- set.populations(GENOME.class, new.populations=pop.list, diploid=TRUE)  
