1b16e70144ecb19a38019851565bdace9edf5894
rtraborn <rtraborn@asu.edu>
2019-12-05 23:50:38 -0500

    Updated script to include another pop set in the hval distribution. Added new plot as well.

--- results/hvalue_dist.R
+++ results/hvalue_dist.R
@@ -1,7 +1,6 @@
-#library(tidyverse)
 library(ggplot2)
 
-setwd("/N/dc2/projects/PromoterPopGen/hval_dist_analysis/PopGen_matrix_test/")
+setwd("/N/dc2/projects/PromoterPopGen/PromoterPopGen/results")
 
 ma_file <- read.table(file="cis1/GBR.txt", header=FALSE)
 
@@ -21,3 +20,45 @@ all_data_plus <- all_data[my.ind]
 
 save(all_data, file="all_hval_data.RData")
 save(all_data_plus, file="all_hval_data_plus.RData")
+
+hval.plus <- as.data.frame(cbind(all_data_plus))
+n.row.vec <- 1:nrow(hval.plus)
+
+hval.plus.df <-  cbind(hval.plus, n.row.vec)
+head(hval.plus.df)
+
+a <- ggplot(hval.plus.df, aes(all_data_plus))
+a + geom_histogram(binwidth = 0.01, col="white", fill="royalblue") 
+
+ggsave(file="h_val_dist_GBR.png")
+
+ma_file <- read.table(file="cis1/FIN.txt", header=FALSE)
+
+ma_data <- ma_file[,-1:-2]
+rownames(ma_data) <- ma_file[,2]
+my.vec <- 0:40000
+my.colnames <- paste("site",my.vec, sep="_")
+colnames(ma_data) <- my.colnames
+
+ma_data[1:10, 1:10]
+
+all_data <- unlist(unclass(ma_data))
+
+my.ind <- which(all_data>0)
+
+all_data_plus <- all_data[my.ind]
+
+save(all_data, file="all_hval_data_FIN.RData")
+save(all_data_plus, file="all_hval_data_plus_FIN.RData")
+
+hval.plus <- as.data.frame(cbind(all_data_plus))
+n.row.vec <- 1:nrow(hval.plus)
+
+hval.plus.df <-  cbind(hval.plus, n.row.vec)
+head(hval.plus.df)
+
+a <- ggplot(hval.plus.df, aes(all_data_plus))
+a + geom_histogram(binwidth = 0.01, col="white", fill="darkgreen") 
+
+ggsave(file="h_val_dist_FIN.png")
+
